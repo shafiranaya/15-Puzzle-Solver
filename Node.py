f0c7956fa4d40c207c3dc1723932992fc87fabf4
shafiranaya <shfrnaya@gmail.com>
2021-07-22 18:13:09 +0700

    algoritma udah fix

--- Node.py
+++ Node.py
@@ -5,7 +5,6 @@ class Node:
     def __init__(self, position, moves, cost):
         self.position = position
         self.moves  = moves # moves so far
-        # self.current_move = current_move
         self.cost = cost # didapat dari fungsi g
 
     def get_position(self):
@@ -14,19 +13,20 @@ class Node:
     def set_position(self, position):
         self.position = position
 
-    # Jumlah ubin tidak kosong yang tidak terdapat pada susunan akhir
-    def get_g(self):
-        jawaban = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
-        count = 0
-        for i in range(4):
-            for j in range(4):
-                if (self.get_position()[i][j] != 0):
-                    if self.get_position()[i][j] != jawaban[i][j]:
-                        count += 1
-                        # print("i,j",i,j)
-        return count
+    # # Jumlah ubin tidak kosong yang tidak terdapat pada susunan akhir
+    # def get_g(self):
+    #     jawaban = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+    #     count = 0
+    #     for i in range(4):
+    #         for j in range(4):
+    #             if (self.get_position()[i][j] != 0):
+    #                 if self.get_position()[i][j] != jawaban[i][j]:
+    #                     count += 1
+    #                     # print("i,j",i,j)
+    #     return count
 
-    def get_h(self):
+    # # Jumlah manhattan distance (jarak antara posisi saat itu dengan posisi seharusnya) ubin tidak kosong yang tidak terdapat pada susunan akhir
+    def get_g(self):
         jawaban = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
         distance = 0
         for i in range(4):
@@ -39,31 +39,15 @@ class Node:
                     j2 = self.get_coordinate_by_value(jawaban[i][j])[1]
                     distance += abs(i1-i2) + abs(j1-j2)
         return distance
-        # return len(self.moves)
     
-    def get_heuristic_distance(self):
-        jawaban = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
-        distance = 0
-        for i in range(4):
-            for j in range(4):
-                position = self.get_position()
-                if (position[i][j] != 0):
-                    i1 = self.get_coordinate_by_value(position[i][j])[0]
-                    j1 = self.get_coordinate_by_value(position[i][j])[1]
-                    i2 = self.get_coordinate_by_value(jawaban[i][j])[0]
-                    j2 = self.get_coordinate_by_value(jawaban[i][j])[1]
-                    distance += abs(i1-i2) + abs(j1-j2)
-        return distance
+    def get_h(self):
+        return len(self.moves)
     
     def get_f(self):
         return self.get_g() + self.get_h()
 
     def get_moves(self):
         return copy.copy(self.moves)
-    
-    # def add_moves(self,move):
-    #     moves = self.get_moves()
-    #     moves.append(move)
 
     def get_coordinate_by_value(self, value):
         for i in range(4):
@@ -71,7 +55,7 @@ class Node:
                 if value == self.position[i][j]:
                     x = i
                     y = j
-        return [x,y]
+        return x, y
 
     def __lt__(self, other):
         return self.get_f() < other.get_f()
@@ -87,27 +71,21 @@ class Node:
         print("Position: ")
         self.print_position()
         print("Moves so far: ", self.moves)
-        # print("Current move/prev move: ", self.current_move)
+        if (self.moves):
+            print("Prev move: ", self.moves[-1])
         print("Cost (nilai g): ",self.get_g())
         print("Heuristic: ",self.get_h())
         print("nilai f:",self.get_f())
-    
+
+    # untuk move sesuai langkah (ganti posisi)
     def move(self,direction):
+        dictionary = {"up":[-1,0],"right":[0,1],"down":[1,0],"left":[0,-1]}
+        delta = dictionary[direction]
+        i = delta[0]
+        j = delta[1]
         i_old, j_old = self.get_coordinate_by_value(0)
-        position = self.get_position()
-        if direction == "down":
-            i = 1
-            j = 0
-        elif direction == "up":
-            i = -1
-            j = 0
-        elif direction == "right":
-            i = 0
-            j = 1
-        elif direction == "left":
-            i = 0
-            j = -1   
+        position = self.get_position() 
         i_new, j_new = i_old + i, j_old + j
         position[i_old][j_old] = position[i_new][j_new]
         position[i_new][j_new] = 0
-        self.set_position(position)
+        self.position = position
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import copy
 from heapq import heapify, heappop, heappush
 from Node import Node
 
-# TODO: ada berapa simpul yang dibangkitkan?
 # Asumsi: susunan_akhir itu terurut
 susunan_awal = [[1,3,4,15],[2,0,5,12],[7,6,11,14],[8,9,10,13]]
 # Note: kalau target, 0 nya itu jadi 16
-susunan_akhir = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+# susunan_akhir = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 jawaban = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
 
 def find_X(susunan):
@@ -41,7 +40,6 @@ def kurang(i, susunan_awal):
                 posisi_j = posisi(j,susunan_awal)
                 posisi_i = posisi(i,susunan_awal)
                 if (j < i) and (posisi_j > posisi_i) and (j != 0):
-                    # print("j = ",j)
                     count += 1
     else:
         posisi_kosong = posisi(0,susunan_awal)
@@ -61,65 +59,36 @@ def is_reachable(susunan_awal):
         reachable = False
     return reachable
 
-# Jumlah ubin tidak kosong yang tidak terdapat pada susunan akhir
-def misplaced(susunan_awal, susunan_akhir):
-    count = 0
-    for i in range(4):
-        for j in range(4):
-            if (susunan_awal[i][j] != 0):
-                if susunan_awal[i][j] != susunan_akhir[i][j]:
-                    count += 1
-    return count
-
 def get_direction(i,j):
-    if (i == 1 and j == 0):
-        move = "down"
-    if (i == -1 and j == 0):
-        move = "up"
-    if (i == 0 and j == 1):
-        move = "right"
-    if (i == 0 and j == -1):
-        move = "left"
-    return move
+    dictionary = {"up":[-1,0],"right":[0,1],"down":[1,0],"left":[0,-1]}
+    key_list = list(dictionary.keys())
+    val_list = list(dictionary.values())
+    delta = [i,j]
+    direction = val_list.index(delta)
+    return key_list[direction]
 
-# Masih bermasalah di moves
 def get_child_nodes(node):
     children = []
     i_old, j_old = node.get_coordinate_by_value(0)
-
-    # ver2: deltas
     # deltas priority: up, right, down, left
     deltas = [[-1,0],[0,1],[1,0],[0,-1]]
     for delta in deltas:
         i = delta[0]
         j = delta[1]
-
-    # ver1 versi biasa
-    # delta = [0,-1,1]
-    # for i in delta:
-    #     for j in delta:
-            # prioritas gerak (-1,0,1): up, left, right, down
-            # prioritas gerak (-1,1,0): up, down, left, right
-            # prioritas gerak (1,0,-1): down, right, left, up
-            # prioritas gerak (1,-1,0): down, up, right, left
-            # prioritas gerak (0,1,-1): right, left, down, up
-            # prioritas gerak (0,-1,1): left, right, up, down
         if i*j != 0 or i == j:
             continue
         i_new, j_new = i_old + i, j_old + j
         if (not (0 <= i_new <= 3)) or (not (0 <= j_new <= 3)):
             continue
-        # print("i,j: ",i,j,get_direction(i,j))
-        move = get_direction(i,j)
+        direction = get_direction(i,j)
         position = node.get_position()
         position[i_old][j_old] = position[i_new][j_new]
         position[i_new][j_new] = 0
         moves = node.get_moves()
-        moves.append(move)
-        # child_move = move
+        moves.append(direction)
         g = node.get_g()
-        # child = Node(position, moves, g(node.get_position(),jawaban))
-        child = Node(position, moves, g)
+        h = node.get_h()
+        child = Node(position, moves, g+h)
         children.append(child)
     return children
 
@@ -133,23 +102,22 @@ def solve(susunan_awal):
         expanded = []
         queue = []
         heapify(queue)
-        node = Node(susunan_awal, [], 0) # TODO costnya ganti ga ya (?)
+        node = Node(susunan_awal, [], 0)
         heappush(queue,node)
         node_count = 0
         iteration = 1
         while (queue):
-            # print("Queue: ",queue)
-            # print("Queue length: ",len(queue))
             current_node = heappop(queue)
             print("--- Iterasi ke-",iteration,"---")
             current_node.print_node()
             iteration += 1
-            if (current_node.get_h() == 0): # TODO ganti
+            if (current_node.get_g() == 0):
                 print("Goal ketemu")
                 print("Queue length: ",len(queue))
                 # print("Solusi (position): ", current_node.get_position())
                 print("Simpul yang dibangkitkan",node_count)
-                return current_node.get_moves()
+                solution = current_node.get_moves()
+                return solution
                 # return moves
             children = get_child_nodes(current_node)
             for child in children:
@@ -169,6 +137,8 @@ def print_solution(node, moves):
         node.print_position()
         i+=1
 
+### DRIVER ###
+
 s_1 = [[1,2,3,4],[5,6,7,8],[9,10,0,11],[13,14,15,12]]
 soal1 = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
 soal2 = [[0,2,3,4],[1,6,7,8],[5,10,11,12],[9,13,14,15]]
@@ -177,66 +147,19 @@ soal4 = [[1,9,4,8],[3,2,6,7],[13,10,11,12],[14,5,15,0]] #
 
 puzzle2 = [[5,1,3,4],[9,2,6,7],[13,10,11,8],[0,14,15,12]]
 puzzle3 = [[5,1,3,4],[9,2,6,7],[13,11,0,8],[14,10,15,12]]
-puzzle4 = [[1,6,7,5],[9,3,10,2],[13,8,4,12],[14,11,15,0]]
+puzzle4 = [[1,6,7,5],[9,3,10,2],[13,8,4,12],[14,11,15,0]] # SUSAH
+puzzle5 = [[1,2,3,4],[5,6,7,0],[9,10,12,8],[11,13,14,15]]
 
-soal_susah = [[1,9,5,6],[2,3,4,10],[7,8,13,15],[11,12,14,0]] #
-# print(g(s_1,susunan_akhir))
-# print("Reachable?", is_reachable(susunan_awal))
-# node = Node(soal, [], g(soal,susunan_akhir))
-# children_node = get_child_nodes(node)
-# for child in children_node:
-#     print("Child")
-#     child.print_node()
-# print(children_node)
+testcase1 = [[1,2,4,7],[5,6,0,3],[9,11,12,8],[13,10,14,15]] # 11 moves, 40 nodes
+testcase2 = [[1,2,12,3],[5,6,8,4],[13,9,11,15],[10,0,7,14]] # 20 moves, 192 nodes
+testcase3 = [[1,2,3,4],[5,6,7,8],[11,12,15,14],[10,9,13,0]] # 16 moves, 45 nodes
+
+soal_susah = [[1,9,5,6],[2,3,4,10],[7,8,13,15],[11,12,14,0]] # SUSAH
+
 print("---BATAS---")
-# # SOAL 1
-# m1 = misplaced(soal1,jawaban)
-# ASTAR
-# iterasi: 4
-# simpul: 10
-# steps: 3
-# # SOAL 2
-# m2 = misplaced(soal2,jawaban)
-# ASTAR
-# iterasi: 7
-# simpul: 12
-# steps: 6
-# # SOAL 3
-# m3 = misplaced(soal3,jawaban)
-# BNB
-# iterasi: 405
-# simpul: 844
-# steps: 17
-# ASTAR
-# iterasi: 534
-# simpul: 1066
-# steps: 83
-# ASTAR REVISI
 
-# # SOAL 4
-# m4 = misplaced(soal4,jawaban)
-# # SOAL SUSAH
-# m5 = misplaced(soal_susah,jawaban)
-# print(m1,m2,m3,m4,m5)
-
-# PUZZLE 2
-# ASTAR
-# iterasi: 10
-# simpul: 20
-# steps: 9
-# PUZZLE 3
-# ASTAR
-# iterasi: 13
-# simpul: 28
-# steps: 12
 moves = solve(puzzle4)
 print("MOVES =",moves)
 node1 = Node(puzzle4, [], 0) # TODO costnya ganti ga ya (?)
 print("Solusi:")
 print_solution(node1, moves)
-
-# print(find_X(susunan_awal))
-# print(posisi(15,susunan_awal,susunan_akhir))
-# print(kurang(15,susunan_awal,susunan_akhir))
-# print(kurang(16,susunan_awal,susunan_akhir))
-# def kurang(i, susunan):
